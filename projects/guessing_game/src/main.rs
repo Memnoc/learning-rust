98ac2d4d90996bda9d8e917041e7cf80fcce93c9
Memnoc <smartdroidesign@gmail.com>
2024-02-28 07:08:07 +0000

    looping through the guesses and offering a break in case of win

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("**************-Guess the number!-**************");
@@ -9,24 +9,26 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please enter your guess");
+    loop {
+        println!("Please enter your guess");
 
-    let mut guess: String = String::new();
+        let mut guess: String = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: i32 = guess
-        .trim()
-        .parse()
-        .expect("Please type a number!");
-    println!("You guessed {guess}");
+        let guess: i32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed {guess}");
 
-    // comparing
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small :( "),
-        Ordering::Greater=> println!("Too big :( "),
-        Ordering::Equal => println!("You win!"),
+        // comparing
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small :( "),
+            Ordering::Greater => println!("Too big :( "),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
