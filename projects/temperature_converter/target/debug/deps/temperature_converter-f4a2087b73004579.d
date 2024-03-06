cf4038f1d7ee3dfd5fbf5eed454e01894087e963
Memnoc <smartdroidesign@gmail.com>
2024-03-06 07:26:53 +0000

    Program | Temperature converter

--- projects/temperature_converter/target/debug/deps/temperature_converter-f4a2087b73004579.d
+++
@@ -1,5 +0,0 @@
-/home/memnoc/Code/rust-book/projects/temperature_converter/target/debug/deps/temperature_converter-f4a2087b73004579: src/main.rs
-
-/home/memnoc/Code/rust-book/projects/temperature_converter/target/debug/deps/temperature_converter-f4a2087b73004579.d: src/main.rs
-
-src/main.rs:
--- projects/temperature_converter/target/debug/temperature_converter.d
+++
@@ -1,1 +0,0 @@
-/home/memnoc/Code/rust-book/projects/temperature_converter/target/debug/temperature_converter: /home/memnoc/Code/rust-book/projects/temperature_converter/src/main.rs
