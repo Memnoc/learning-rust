32614c22d80e472a83f1171b8e56297e7279eff8
Memnoc <smartdroidesign@gmail.com>
2024-03-08 07:41:22 +0000

    Program | Fibonacci nth generator - refactoring to match and DRYer code

--- projects/fibonacci_generator/src/main.rs
+++ projects/fibonacci_generator/src/main.rs
@@ -1,14 +1,15 @@
-fn calculate_fibonacci(number: i32) -> i32 {
-    if number <= 1 {
-        number
-    } else {
-        calculate_fibonacci(number - 1) + calculate_fibonacci(number - 2)
+fn calculate_fibonacci(number: u32) -> u32 {
+    match number {
+        0 => 0,
+        1 => 1,
+        _ => calculate_fibonacci(number -1) + calculate_fibonacci(number - 2),
+
     }
 }
 
 fn main() {
     println!("****** nth Fibonacci generator ******");
-    let n: i32 = 10;
+    let n = 10;
     let fibonacci_number = calculate_fibonacci(n);
     println!("The {}th Fibonacci number is: {}", n, fibonacci_number);
 }
