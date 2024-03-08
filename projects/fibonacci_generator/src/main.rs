d06a7309555da4a3da6268af410e4225c241d239
Memnoc <smartdroidesign@gmail.com>
2024-03-08 07:29:00 +0000

    Program | Fibonacci nth generator

--- projects/fibonacci_generator/src/main.rs
+++ projects/fibonacci_generator/src/main.rs
@@ -1,3 +1,14 @@
+fn calculate_fibonacci(number: i32) -> i32 {
+    if number <= 1 {
+        number
+    } else {
+        calculate_fibonacci(number - 1) + calculate_fibonacci(number - 2)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("****** nth Fibonacci generator ******");
+    let n: i32 = 10;
+    let fibonacci_number = calculate_fibonacci(n);
+    println!("The {}th Fibonacci number is: {}", n, fibonacci_number);
 }
---
+++ projects/fibonacci_generator/target/debug/fibonacci_generator.d
@@ -0,0 +1,1 @@
+/home/memnoc/Code/rust-book/projects/fibonacci_generator/target/debug/fibonacci_generator: /home/memnoc/Code/rust-book/projects/fibonacci_generator/src/main.rs
